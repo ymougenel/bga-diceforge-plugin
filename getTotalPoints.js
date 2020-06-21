e283536adc64c7380015085ead4b052628dba18b
ymougenel <yann.mougenel@gmail.com>
2020-06-21 16:35:18 +0200

    v1: total points working

--- getTotalPoints.js
+++ getTotalPoints.js
@@ -1,88 +1,94 @@
 // ==UserScript==
-// @name         dice forge
+// @name         dice forge - points counter
 // @namespace    https://boardgamearena.com/
-// @version      0.1
+// @version      1.0
 // @description  Lets see
 // @author       slonepi
 // @match        https://boardgamearena.com/*
 // @grant        none
 // ==/UserScript==
 
-
-(function() {
-    'use strict';
-
-    // Your code here...
-    console.log('123');
-})();
-
-
-var counters=document.getElementsByClassName("card-counter");
-var pointsInit=false;
-
-
+// Periodic points update
 var i = setInterval(function() {
-    console.log('~~ Hello World');
-    if(! pointsInit) {
-        initPoints();
-    }
     updatePlayerPoints(0);
     updatePlayerPoints(1);
+}, 2000);
 
-}, 10000);
 
-
+/**
+ * Update player total ploints
+ *
+ * @input player turn order (either 0 or 1)
+ */
 function updatePlayerPoints(player) {
-  let pointsArea = document.getElementById("cardPoints"+player);
-  if (pointsArea) {
-    pointsArea.innerText = getCardsPoints(player)
+  let totalPointsArea = document.getElementById("totalPoints"+player);
+  if (!totalPointsArea) {
+    initPoints(player);
+    totalPointsArea = document.getElementById("totalPoints"+player);
   }
+  let cardsPoints = getCardsPoints(player);
+  let dicePoints = document.getElementsByClassName("player_score_value")[player].innerText;
+  let totalPoints = Number(dicePoints) + Number(cardsPoints);
+  console.log("Total points of player:" + player + "=" + totalPoints);
+  totalPointsArea.innerText = totalPoints + " PV";
 }
 
-// Init the player points
-function initPoints(){
-    console.log('~~ Init point CardPoints...');
-    let cardSpanPoints = document.getElementsByClassName("player_score_value");
-    console.log(cardSpanPoints.length);
-    if (cardSpanPoints.length > 0) {
-        console.log('~~ game started, inititialing');
-        pointsInit=true;
-        console.log(cardSpanPoints[0]);
-        cardSpanPoints[0].appendChild(createText(0,'0'));
-        cardSpanPoints[1].appendChild(createText(1,'0'));
-    }
+/**
+ * Init the player points HTML area
+ *
+ * @input player turn order (either 0 or 1)
+ */
+ function initPoints(player) {
+    let totalPoints = document.getElementsByClassName("player-name");
+    totalPoints[player].appendChild(createTotalPoints(player,'0 PV'));
 }
 
-// Prepare Score location
-function createText(player, value) {
+/**
+ * Create the player points HTML area
+ *
+ * @input player turn order (either 0 or 1)
+ */
+function createTotalPoints(player, value) {
     let para = document.createElement("P");
     let t = document.createTextNode(value);
-    para.id='cardPoints'+player
+    para.id='totalPoints'+player
     para.appendChild(t);
     return para;
 }
 
+/**
+ * Count the player card points
+ *
+ * @input player turn order (either 0 or 1)
+ * @output the player card points
+ */
 function getCardsPoints(player) {
   let cards = document.getElementsByClassName("cards-pile")[player].getElementsByClassName("exploit")
   let points = 0;
   for (let card of cards) {
+    // Cards are regrouped in a file, the displayed sprite depends on the background postions
     let i = formatCoodinate(getComputedStyle(card).backgroundPositionY)
     let j = formatCoodinate(getComputedStyle(card).backgroundPositionX)
-    console.log("i=" + i + " j=" + j)
-    console.log("points="+ cardPoints[i][j-1])
-    points += cardPoints[i][j-1]
+    points += cardPoints[i][j]
   }
   return points
 }
 
+/**
+ * Transform the background style into matrix coordinate
+ *
+ * (example: "-500%" -> 5)
+ * @input the background style
+ * @output the coordinate
+ */
 function formatCoodinate(cor) {
   return Number(cor.substring(0, cor.length - 1)) / (- 100)
 }
 
 var cardPoints = [
-[0,NaN,2,NaN,4,NaN,8,NaN,14,NaN,10],
-[NaN,10,NaN,26,NaN,8,NaN,4,NaN,12,NaN],
-[6,NaN,2,NaN,2,NaN,0,NaN,4,NaN,6],
-[NaN,8,NaN,8,NaN,16,NaN,6,NaN,6,NaN],
-[4,NaN,4,NaN,4,NaN,4,NaN,2,NaN,27]
+[NaN,0,NaN,2,NaN,4,NaN,8,NaN,14,NaN],
+[10,NaN,10,NaN,26,NaN,8,NaN,4,NaN,12],
+[NaN,6,NaN,2,NaN,2,NaN,0,NaN,4,NaN],
+[6,NaN,8,NaN,8,NaN,16,NaN,6,NaN,6],
+[NaN,4,NaN,4,NaN,4,NaN,4,NaN,2,NaN]
 ]
